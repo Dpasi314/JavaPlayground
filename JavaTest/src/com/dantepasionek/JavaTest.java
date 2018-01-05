c1b5ae47c792f1a44de30e1da83979c5f6631986
Dante Pasionek <dante.pasionek1@gmail.com>
2018-01-04 19:00:24 -0700

    Initial commit

---
+++ JavaTest/src/com/dantepasionek/JavaTest.java
@@ -0,0 +1,8 @@
+package com.dantepasionek;
+
+public class JavaTest {
+ public static void main(String[] args) {
+ 	System.out.println("Hello World!");
+ }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# JavaPlayground
+
+It's been a while since I've made any program in Java. So here's my playground to un-rust those gears!
